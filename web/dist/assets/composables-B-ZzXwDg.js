09c550aae058763dc2bc3f79b121bf979b7dc4e5
mr.woods1692 <mr.woods1692@gmail.com>
2026-09-13 16:11:46 +0800

    chore(web): 提交 /settings 页对应的构建产物
    Settings-BjSlzq34.js 为新增 chunk；Profile-CS0o_F2Q.js 已不含「编辑资料」面板；index 内含 /settings 路由注册。

--- web/dist/assets/composables-B-ZzXwDg.js
+++
@@ -1,1 +0,0 @@
-import{o as a,q as o}from"./index-E75m2PYY.js";function u(){a(async()=>{await o.me()})}export{u};
---
+++ web/dist/assets/composables-eoP5tju0.js
@@ -0,0 +1,1 @@
+import{o as a,q as o}from"./index-CudgdWiz.js";function u(){a(async()=>{await o.me()})}export{u};
